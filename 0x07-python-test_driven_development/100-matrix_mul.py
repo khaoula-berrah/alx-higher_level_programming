217974fc822a3c1c581e0344bb867d9577b4d5c8
khaoula-berrah <berah.khaoula@gmail.com>
2023-07-05 02:11:07 -0700

    test

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a matrix multiplication function."""
 
+
 def matrix_mul(m_a, m_b):
     """Multiply two matrices.
     Args:
@@ -62,4 +63,4 @@ def matrix_mul(m_a, m_b):
             new_row.append(prod)
         new_matrix.append(new_row)
 
-    return (new_matrix)
+   return new_matrix
