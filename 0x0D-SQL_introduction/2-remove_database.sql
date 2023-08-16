f3577b57508277ff4bb08b3c7c7602e7a3d488d4
khaoula-berrah <berah.khaoula@gmail.com>
2023-08-16 09:05:07 -0700

    sql

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,5 @@
 -- Deletes the database hbtn_0c_0 from my MySQL server.
-DROP DATABASE IF EXISTS `hbtn_0c_0`;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
